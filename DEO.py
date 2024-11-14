42aca65ba970402c81c29762be3d758c4a4f28c6
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-11-14 18:55:49 +0100

    Initial

---
+++ DEO.py
@@ -0,0 +1,9 @@
+dic = {"Name":"Reostat","Age":11,"Grade":6}
+print(dic)
+print("Name is",dic["Name"])
+print("Age is",dic["Age"])
+print("Grade is",dic["Grade"])
+dic['Gender'] = "Male"
+print(dic)
+dic.pop("Grade")
+print(dic)
